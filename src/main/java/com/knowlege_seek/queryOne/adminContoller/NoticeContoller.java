fe4ff957d12c70219cebc519e65f48a82bf925df
DaeKyoung <jdk7167@naver.com>
2016-09-23 12:26:53 +0900

    gg

--- src/main/java/com/knowlege_seek/queryOne/adminContoller/NoticeContoller.java
+++ src/main/java/com/knowlege_seek/queryOne/adminContoller/NoticeContoller.java
@@ -12,6 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -81,7 +82,8 @@ public class NoticeContoller {
 	public String writeForm(){
 		return "/admin/noticeWrite";
 	}
-
+	
+	@Transactional
 	@RequestMapping(value ="/write.do",method =RequestMethod.POST)
 	public String write(Notice notice){
 
