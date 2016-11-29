b21e8e7ea1b869ebb2a0c9b82f02c4491329914b
DaeKyoung <jdk7167@naver.com>
2016-11-29 18:15:46 +0900

    url 매핑 patch 삭제

--- src/main/java/com/knowledge_seek/queryOne/userController/HomeController.java
+++ src/main/java/com/knowledge_seek/queryOne/userController/HomeController.java
@@ -1,5 +1,9 @@
 package com.knowledge_seek.queryOne.userController;
 
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.List;
@@ -7,14 +11,21 @@ import java.util.Locale;
 import java.util.Map;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 import org.hamcrest.core.IsNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.knowledge_seek.queryOne.domain.Download;
 import com.knowledge_seek.queryOne.domain.Licencekey;
@@ -144,6 +155,61 @@ public class HomeController {
 		
 		return "/user/qna";
 	}
+//특정 경로에 ftp로 업로드한 파일 찾아 파일 다운로드
+		@RequestMapping("/{file_name:.+}")
+		@ResponseBody
+		public ResponseEntity<FileSystemResource> ftp_down(@PathVariable("file_name") String file_name, HttpServletRequest req){
+			
+			File file = new File( "/home/queryOne/"+file_name);
+			//File file = new File( "D:/queryOne/upload/"+file_name);
+			
+			
+			System.out.println("file:"+file.getPath());
+			HttpHeaders header = new HttpHeaders();
+			header.setContentLength(file.length());
+			//whoAmi = Thread.currentThread().getStackTrace()[1].toString();
+			String fileName="";
+			ResponseEntity<FileSystemResource> entity = null;
+			try {
+				
+				fileName = URLEncoder.encode(file_name, "UTF-8");
+				if(req!=null){
+					String userAgent=req.getHeader("user-agent");
+					 if(userAgent.contains("Trident")){//IE 일 때
+					fileName = URLEncoder.encode(file_name, "EUC-KR");
+					
+					 }
+				}
+				
+				fileName = URLDecoder.decode(fileName, "ISO8859_1");
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+				
+			}
+			try {
+				/*if(ext.equals("zip")){
+					header.set("Content-Type", "application/zip");
+				  else{
+				  	header.set("Content-Type", "application/xml");
+				  }
+				}*/
+				header.set("Content-disposition", "attachment; filename="+ fileName);
+				System.out.println("file.exists():"+file.exists());
+			if(file.exists()){
+				
+				entity = new ResponseEntity<FileSystemResource>(new FileSystemResource(file), header, HttpStatus.OK);
+			}
+				else{
+					entity = new ResponseEntity<FileSystemResource>(new FileSystemResource(file), header, HttpStatus.NOT_FOUND);
+				}
+			} catch (Exception e) {
+
+				e.printStackTrace();
+
+			}
+			return entity;
+			
+		}
 	
 	
 	
